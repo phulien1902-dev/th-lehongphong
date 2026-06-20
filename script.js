363c70c4ae3a5898079dc690404d14256dc70ed0
phulien1902-dev <phulien1902@gmail.com>
2026-06-20 15:24:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const searchBox=document.getElementById("searchBox");
 
+searchBox.addEventListener("keyup",function(){
+
+let value=this.value.toLowerCase();
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+if(card.innerText.toLowerCase().includes(value))
+card.style.display="block";
+
+else
+card.style.display="none";
+
+});
+
+});

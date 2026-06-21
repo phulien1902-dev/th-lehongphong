acd0dad11ca73a995850c3cbf9287589a65f6218
phulien1902-dev <phulien1902@gmail.com>
2026-06-22 05:55:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ alert(document.body.className);
     }
 
 });
+// ĐỒNG HỒ
+
+function updateClock(){
+
+    let now = new Date();
+
+    let time = now.toLocaleTimeString('vi-VN');
+
+    let date = now.toLocaleDateString('vi-VN');
+
+    document.getElementById("clock").innerHTML =
+        "🕒 " + time + "<br>📅 " + date;
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();

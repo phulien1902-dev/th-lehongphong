9d22cc79f0879a6a7270fad8897cd5b163bba1e9
phulien1902-dev <phulien1902@gmail.com>
2026-06-21 22:42:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // SEARCH
-    const searchBox = document.getElementById("searchBox");
-
-    if (searchBox) {
-
-        searchBox.addEventListener("input", function () {
-
-            let keyword = this.value.toLowerCase();
-
-            let cards = document.querySelectorAll(".card");
-
-            cards.forEach(function (card) {
-
-                if (card.innerText.toLowerCase().includes(keyword)) {
-
-                    card.style.display = "";
-
-                } else {
-
-                    card.style.display = "none";
-
-                }
-
-            });
-
-        });
-
-    }
-
-    // DARK MODE
     const darkModeBtn = document.getElementById("darkModeBtn");
 
     if (darkModeBtn) {
 
         darkModeBtn.addEventListener("click", function () {
 
-            document.body.classList.toggle("dark-mode");
+            alert("Đã bấm nút Chế độ tối");
 
         });
 

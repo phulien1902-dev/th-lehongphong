ced65f92bfca45d1ead598a3e7d0d3bd32668912
phulien1902-dev <phulien1902@gmail.com>
2026-06-22 21:50:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -217,3 +217,54 @@ window.addEventListener("load", function(){
     }
 
 });
+// SLIDER
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+slides.forEach(slide => {
+
+slide.classList.remove("active");
+
+});
+
+slides[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+currentSlide++;
+
+if(currentSlide >= slides.length){
+
+currentSlide = 0;
+
+}
+
+showSlide(currentSlide);
+
+}
+
+function prevSlide(){
+
+currentSlide--;
+
+if(currentSlide < 0){
+
+currentSlide = slides.length - 1;
+
+}
+
+showSlide(currentSlide);
+
+}
+
+setInterval(nextSlide,4000);
+
+document.querySelector(".next").onclick = nextSlide;
+
+document.querySelector(".prev").onclick = prevSlide;

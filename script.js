38ead571eb3a8ecb0ebe7b0e08b0756f27fcf4d0
phulien1902-dev <phulien1902@gmail.com>
2026-06-22 21:07:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -147,3 +147,54 @@ window.addEventListener("scroll", function () {
         progress + "%";
 
 });
+// PARTICLES
+
+particlesJS("particles-js", {
+
+  particles: {
+
+    number: {
+      value: 80
+    },
+
+    color: {
+      value: "#ffffff"
+    },
+
+    shape: {
+      type: "circle"
+    },
+
+    opacity: {
+      value: 0.3
+    },
+
+    size: {
+      value: 3
+    },
+
+    line_linked: {
+
+      enable: true,
+
+      distance: 150,
+
+      color: "#ffffff",
+
+      opacity: 0.2,
+
+      width: 1
+
+    },
+
+    move: {
+
+      enable: true,
+
+      speed: 2
+
+    }
+
+  }
+
+});
